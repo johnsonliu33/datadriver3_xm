814c2848fadd01594da15a575668b10b4da11df2
hzwflying <110674363@qq.com>
2019-08-03 18:33:02 +0800

    add for a project

---
+++ data_drivern_by_excel_log.py
@@ -0,0 +1,80 @@
+﻿# encoding=utf-8
+from selenium import webdriver
+import unittest, time
+import logging, traceback
+import ddt
+from xm.ExcelUtil import ParseExcel
+from selenium.common.exceptions import NoSuchElementException
+
+"""
+ddt单元测试，不用for循环，以元组方式获取数据
+"""
+
+
+# 初始化日志对象
+logging.basicConfig(
+    # 日志级别
+    level = logging.INFO,
+    # 日志格式
+    # 时间、代码所在文件名、代码行号、日志级别名字、日志信息
+    format = '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+    # 打印日志的时间
+    datefmt = '%a, %Y-%m-%d %H:%M:%S',
+    # 日志文件存放的目录（目录必须存在）及日志文件名
+    filename = 'e:/dataDriveRreport.log',
+    # 打开日志文件的方式
+    filemode = 'w'
+)
+
+excelPath = 'data.xlsx'
+sheetName = "搜索数据表"
+# 创建ParseExcel类的实例对象
+excel = ParseExcel(excelPath, sheetName)
+
+@ddt.ddt
+class TestDemo(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    @ddt.data(*excel.getDatasFromSheet())
+    def test_dataDrivenByFile(self, data):
+        testData, expectData = tuple(data)
+        url = "http://www.baidu.com"
+        # 访问百度首页
+        self.driver.get(url)
+        # 将浏览器窗口最大化
+        self.driver.maximize_window()
+        print (testData, expectData)
+        # 设置隐式等待时间为10秒
+        self.driver.implicitly_wait(10)
+
+        try:
+            # 获取当前的时间戳，用于后面计算查询耗时用
+            start = time.time()
+            # 获取当前时间的字符串，表示测试开始时间
+            startTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            # 找到搜索输入框，并输入测试数据
+            self.driver.find_element_by_id("kw").send_keys(testData)
+            # 找到搜索按钮，并点击
+            self.driver.find_element_by_id("su").click()
+            time.sleep(3)
+            # 断言期望结果是否出现在页面源代码中
+            self.assertTrue(expectData in self.driver.page_source)
+            print ("搜索%s，期望%s" %(testData, expectData))
+        except NoSuchElementException as e:
+            logging.error("查找的页面元素不存在，异常堆栈信息："\
+                          + str(traceback.format_exc()))
+        except AssertionError as e:
+            print("断言失败了")
+            logging.info("搜索“%s”，期望“%s”，失败" %(testData, expectData))
+        except Exception as e:
+            logging.error("未知错误，错误信息：" + str(traceback.format_exc()))
+        else:
+            logging.info("搜索“%s”，期望“%s”通过" %(testData, expectData))
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ data_drivern_by_excel_reback_excel.py
@@ -0,0 +1,38 @@
+﻿#encoding=utf-8
+from selenium import webdriver
+import time
+import datetime
+from openpyxl import *
+wb=load_workbook('data.xlsx')#加载如内存
+ws=wb.active  #获取第一个sheet
+print ("最大行号:",ws.max_row)
+
+"""
+把数据写回excel
+"""
+
+#excel行是从1开始的，所以我们从2开始迭代遍历
+#且使用切片，必须有结束行的索引号，不能写为[1:]，这样不行
+#列号是从0开始的，列取出来是个元组
+driver=webdriver.Chrome()
+test_result=[]
+for row in ws[2:ws.max_row]:
+    print (row[1].value,row[2].value)
+    try:
+        driver.maximize_window()
+        driver.get("http://www.baidu.com")
+        driver.find_element_by_id("kw").send_keys(row[1].value)
+        driver.find_element_by_id("su").click()
+        time.sleep(3)
+        assert row[2].value in driver.page_source
+        row[3].value=time.strftime('%Y-%m-%d %H:%M:%S')
+        row[4].value="成功"
+    except AssertionError as e:
+        row[3].value=time.strftime('%Y-%m-%d %H:%M:%S')
+        row[4].value="断言失败"
+    except Exception as e:
+        row[3].value=time.strftime('%Y-%m-%d %H:%M:%S')
+        row[4].value="出现异常失败"
+
+driver.quit()
+wb.save("data.xlsx")  #注意：将直接覆盖，不是更新。
